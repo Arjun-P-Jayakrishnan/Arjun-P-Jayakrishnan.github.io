9161fcec406287fc77b085a3a0aa1886f9b6f086
Arjun-P-Jayakrishnan <arjunpjayakrishnan@gmail.com>
2024-07-14 23:56:01 +0530

    fix: changes to overall ui and profile pic

--- script.js
+++ script.js
@@ -4,8 +4,35 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  
+
   /*Add or Remove open class*/
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+/*
+ *Open the pdf
+ * */
+function openPDF() {
+  window.open("./assets/resume.pdf");
+}
+
+/*
+ * Navigate to contacts
+ */
+function navigateToContact() {
+  location.href = "./#contact";
+}
+
+function navigate(site) {
+  switch (site) {
+    case "github":
+      location.href = "https://github.com/Arjun-P-Jayakrishnan";
+      break;
+    case "linkedin":
+      location.href = "https://www.linkedin.com/in/arjun-p-jayakrishnan-5b971b244";
+      break;
+    default:
+      break;
+  }
 }
